c4f3d01151335cd2cbfada47b2ac894da7896ef0
Iurie Chigai <124267735+IurieChi@users.noreply.github.com>
2024-11-07 17:46:19 +0100

    def top 10 rezults function done

---
+++ main.py
@@ -0,0 +1,13 @@
+from googlesearch import search
+
+def top_search_results(query, num_results=10):
+    search_results = search(query, num_results=num_results)
+    return search_results
+
+if __name__ == "__main__":
+    search_query = input("Enter your search query: ")
+    top_results = top_search_results(search_query, num_results=10)
+
+    print("Top 10 search results:")
+    for idx, result in enumerate(top_results, 1):
+        print(f"{idx}. {result}")
